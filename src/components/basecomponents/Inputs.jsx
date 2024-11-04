a18a1f2aee9cf59671548b2302807283e083b010
Tsegay <tsegaygebrezgi56@gmail.com>
2024-11-04 10:16:24 +0300

    add this also

---
+++ src/components/basecomponents/Inputs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+//pass the id labe and all props from user parent component
+const Inputs = () => {
+  return <div>Inputs</div>;
+};
+
+export default Inputs;
---
+++ src/components/hooks/Checkinput.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export function useinput(defaultvalue) {
+  const [enterdvalue, setenteredvalue] = useState(defaultvalue);
+
+  function handleinput() {}
+
+  //then return the values here to the component which is using this
+}
