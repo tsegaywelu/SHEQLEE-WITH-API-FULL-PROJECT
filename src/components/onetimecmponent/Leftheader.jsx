2a3d3b27b64ad901ec331c221e16af74592b910b
Tsegay <tsegaygebrezgi56@gmail.com>
2024-12-28 19:29:26 +0300

    why

--- src/components/onetimecmponent/Leftheader.jsx
+++ src/components/onetimecmponent/Leftheader.jsx
@@ -9,7 +9,6 @@ const Leftheader = () => {
         <h1 className="text-sm md:text-2xl font-extrabold flex justify-center items-center font-recolta">
           Sheqlee
         </h1>
-        <div className="flex flex-col w-full bg-red-900"></div>
       </div>
     </>
   );
