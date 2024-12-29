50dc9039e280ad2fc633b03c3d0f42f430a62262
Tsegay <tsegaygebrezgi56@gmail.com>
2024-12-29 18:03:13 +0300

    show this

--- src/components/onetimecmponent/SalaryInput.jsx
+++ src/components/onetimecmponent/SalaryInput.jsx
@@ -10,18 +10,19 @@ const SalaryInput = ({
   setCurrency,
   setRate,
 }) => {
+  const [isCurrencyDropdownOpen, setIsCurrencyDropdownOpen] = useState(false);
+  const [isRateDropdownOpen, setIsRateDropdownOpen] = useState(false);
+
   return (
     <div className="">
-      {/* <label htmlFor="salary" className="text-xl   my-3 font-kantumruy font-medium text-start">
-        Salary <span className="text-sm text-gray-400">(fixed or range)</span>
-      </label> */}
       <div className="flex items-center  justify-between w-full">
         {/* Currency Dropdown */}
         <div className="relative  w-1/6">
           <select
-            className="appearance-none bg-black text-white text-xl  p-3  rounded-l-md w-full font-kantumruy"
+            className="appearance-none bg-black text-white text-xl  px-3 py-[14px]  rounded-l-md w-full font-kantumruy"
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
+            onClick={() => setIsCurrencyDropdownOpen((prevState) => !prevState)}
           >
             <option value="$">$</option>
             <option value="€">€</option>
@@ -35,8 +36,11 @@ const SalaryInput = ({
               alt=""
               width={10}
               height={10}
-              // className="absolute right-0 top-1/2  transform -translate-y-1/2 pointer-events-none "
-              className={``}
+              className={
+                isCurrencyDropdownOpen
+                  ? "rotate-180 transition duration-700"
+                  : "transition duration-700"
+              }
             />
           </div>
         </div>
@@ -54,9 +58,10 @@ const SalaryInput = ({
         {/* Rate Dropdown */}
         <div className="relative w-1/3 font-kantumruy ">
           <select
-            className="appearance-none bg-black text-white py-3 rounded-r-md p-4 w-full text-lg  font-kantumruy"
+            className="appearance-none bg-black text-white py-[14px] rounded-r-md p-4 w-full text-lg  font-kantumruy"
             value={rate}
             onChange={(e) => setRate(e.target.value)}
+            onClick={() => setIsRateDropdownOpen((prevState) => !prevState)}
           >
             <option value="/hour">/hour</option>
             <option value="/day">/day</option>
@@ -70,7 +75,11 @@ const SalaryInput = ({
               alt=""
               width={10}
               height={10}
-              // className="absolute right-0 top-1/2  transform -translate-y-1/2 pointer-events-none "
+              className={
+                isRateDropdownOpen
+                  ? "rotate-180 transition duration-700"
+                  : "transition duration-700"
+              }
             />
           </div>
         </div>
