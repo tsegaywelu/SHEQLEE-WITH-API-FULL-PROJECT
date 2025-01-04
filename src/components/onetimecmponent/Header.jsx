f615823d53f7c160f97348129ba9803afb9e5466
Tsegay <tsegaygebrezgi56@gmail.com>
2025-01-04 09:02:42 +0300

    what about no

--- src/components/onetimecmponent/Header.jsx
+++ src/components/onetimecmponent/Header.jsx
@@ -8,19 +8,19 @@ const Header = () => {
   // right component
 
   return (
-    <div className="flex justify-between items-center bg-custom-slate ">
+    <div>
       {/* Main container */}
       <div className="bg-custom-slate   flex max-sm:flex-row-reverse  max-sm:justify-end max-sm:py-4 max-sm:px-4 justify-between items-center py-1 md:py-2  lg:py-4 px-2 md:px-5 lg:px-10   lg:rounded-lg">
         {/* left side part */}
+        <div className="font-kantumruy text-sm md:hidden flex justify-end items-center gap-2  w-full ">
+          <button className="font-semibold">Log in</button>
+          <button className="bg-custom-purple py-[2px] px-[6px] rounded-[5px] text-white font-semibold">
+            Sign up
+          </button>
+        </div>
         <Leftheader />
         <Rightheader />
         {/* wright side part */}
-      </div>
-      <div className="font-kantumruy text-sm md:hidden flex justify-center items-center gap-2 px-4">
-        <button className="font-semibold">Log in</button>
-        <button className="bg-custom-purple py-[2px] px-[6px] rounded-[5px] text-white font-semibold">
-          Sign up
-        </button>
       </div>
     </div>
   );
--- src/components/onetimecmponent/Leftheader.jsx
+++ src/components/onetimecmponent/Leftheader.jsx
@@ -6,13 +6,13 @@ const Leftheader = () => {
     <>
       {/* Left section: Logo and title */}
       <Link to={"/sheqlee"}>
-        <div className="flex flex-col md:flex-row  mx-3  md:mx-5 lg:mx-10 gap-5 ">
+        <div className="flex  items-center   mx-3  md:mx-5 lg:mx-10 gap-5 ">
           <img
             src={mysvg}
             alt="Logo loading..."
-            className=" max-sm:h-[30px] max-sm:w-[30px] h-[50px] w-[50px]"
+            className=" h-[50px] w-[50px]"
           />
-          <h1 className="max-sm:hidden md:text-2xl font-extrabold flex justify-center items-center font-recolta">
+          <h1 className=" max-sm:hidden  md:inline  md:text-2xl font-extrabold flex justify-center items-center font-recolta">
             Sheqlee
           </h1>
         </div>
