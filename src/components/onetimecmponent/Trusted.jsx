fa69d602f4b72ac9552f92d188a94239e028e87b
Tsegay <tsegaygebrezgi56@gmail.com>
2025-01-14 21:46:12 +0300

    trusted working now

--- src/components/onetimecmponent/Trusted.jsx
+++ src/components/onetimecmponent/Trusted.jsx
@@ -14,10 +14,26 @@ const Trusted = () => {
         </h2>
       </div>
       <div className=" flex justify-center items-center  gap-10  md:gap-20  ">
-        <img src={google} alt="logs of company" height={40} width={40} />
-        <img src={meta} alt="logs of company" height={40} width={40} />
-        <img src={microsoft} alt="logs of company" height={40} width={40} />
-        <img src={apple} alt="logs of company" height={40} width={40} />
+        <img
+          src={google}
+          alt="logs of company"
+          className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]"
+        />
+        <img
+          src={meta}
+          alt="logs of company"
+          className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]"
+        />
+        <img
+          src={microsoft}
+          alt="logs of company"
+          className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]"
+        />
+        <img
+          src={apple}
+          alt="logs of company"
+          className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]"
+        />
       </div>
     </div>
   );
